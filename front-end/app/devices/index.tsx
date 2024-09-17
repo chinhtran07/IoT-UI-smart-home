3baa43c2ee03adf006038070be5cd7238c6d14a4
chinh-tran <chinhtran07@gmail.com>
2024-09-18 01:03:26 +0700

    Merge remote-tracking branch 'refs/remotes/origin/main'

---
+++ front-end/app/devices/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function DeviceScreen() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    );
+  }
+  
